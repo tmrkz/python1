79edb03f31b1a5d6da7dcc322e960f8c7ffaa5fc
tmrkz <tim.cuz.201@mail.ru>
2021-09-15 12:35:20 +0300

    Second commit

--- p1.py
+++ p1.py
@@ -1,14 +1,15 @@
-a=int(input())
-b=int(input())
-c=int(input())
-D=b * b - 4 * a * c
-if D>0:
-    x1= (-b+(D)**0.5)/2*a
-    x2= (-b-(D)**0.5)/2*a
-    print(x1,' ',x2)
-elif D==0:
-    x= (-b)/2*a
-    print(x)
-else:
-    print('Нет корней!')
+while True:
+    a=int(input())
+    b=int(input())
+    c=int(input())
+    D=b * b - 4 * a * c
+    if D>0:
+        x1= (-b+(D)**0.5)/2*a
+        x2= (-b-(D)**0.5)/2*a
+        print(x1,' ',x2)
+    elif D==0:
+        x= (-b)/2*a
+        print(x)
+    else:
+        print('Нет корней!')
 
